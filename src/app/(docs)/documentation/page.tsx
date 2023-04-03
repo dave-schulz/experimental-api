20306ea457c0a9459221192c70a34468894c2edd
Davey Schulz <daveschulz@hotmail.nl>
2023-04-03 20:25:50 +0200

    Added documentation page with an animated code example

---
+++ src/app/(docs)/documentation/page.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+
+import type { Metadata } from 'next';
+import Heading from '@/components/layout/Heading';
+import Paragraph from '@/components/layout/Paragraph';
+import DocumentationTabs from '@/components/DocumentationTabs';
+
+import 'simplebar-react/dist/simplebar.min.css';
+
+export const metadata: Metadata = {
+  title: 'Similarity API | Documentation',
+  description: 'Similarity API | Documentation',
+};
+
+const page: FC = () => {
+  return (
+    <div className="container max-w-7xl mx-auto mt-12">
+      <div className="flex flex-col items-center gap-6">
+        <Heading>Making a request</Heading>
+        <Paragraph>api/v1/similarity</Paragraph>
+
+        <DocumentationTabs />
+      </div>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/DocumentationTabs.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { FC } from 'react';
+import { Tabs, TabsTrigger, TabsList, TabsContent } from './layout/Tabs';
+import SimpleBar from 'simplebar-react';
+import Code from './Code';
+import { nodejs, python } from '@/helpers/documentation-code';
+
+const DocumentationTabs: FC = () => {
+  return (
+    <Tabs defaultValue="nodejs" className="max-w-2xl w-full">
+      <TabsList>
+        <TabsTrigger value="nodejs">NodeJS</TabsTrigger>
+        <TabsTrigger value="python">Python</TabsTrigger>
+      </TabsList>
+      <TabsContent value="nodejs">
+        <SimpleBar forceVisible="y">
+          <Code animated code={nodejs} language="javascript" show />
+        </SimpleBar>
+      </TabsContent>
+      <TabsContent value="python">
+        <SimpleBar forceVisible="y">
+          <Code animated code={python} language="python" show />
+        </SimpleBar>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+export default DocumentationTabs;
